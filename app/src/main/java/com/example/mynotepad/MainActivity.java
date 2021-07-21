36862fb4ee3e7bfa227c27f3f8b64a88d41b6fe6
mys_ya_ <7nacu7@gmail.com>
2021-07-21 16:51:46 +0300

    ver 0.02 +Создайте класс данных со структурой заметок: название заметки, описание заметки, дата создания.

--- app/src/main/java/com/example/mynotepad/MainActivity.java
+++ app/src/main/java/com/example/mynotepad/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotes();
+
+    }
+
+    private void createNotes() {
+        Note note1 = new Note("Покупки","Что-то купить","01.07.2021");
+        Note note2 = new Note("Учеба","Сделай домашку!","21.07.2021");
+        Note note3 = new Note("Идеи","А что если сделать так:","13.07.2021");
     }
 }
---
+++ app/src/main/java/com/example/mynotepad/Note.java
@@ -0,0 +1,17 @@
+package com.example.mynotepad;
+
+public class Note {
+    String title;
+    String description;
+    String dateCreate;
+
+    public Note() {
+
+    }
+
+    public Note(String title, String description, String date) {
+        this.title = title;
+        this.description = description;
+        this.dateCreate = date;
+    }
+}
