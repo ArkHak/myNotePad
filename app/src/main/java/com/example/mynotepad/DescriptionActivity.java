ccf665ac65a680545a00a0af4994a11e84231e20
mys_ya_ <7nacu7@gmail.com>
2021-07-27 11:41:45 +0300

    ver 0.13 + добавлено меню на все активити+ + в landscape

--- app/src/main/java/com/example/mynotepad/DescriptionActivity.java
+++ app/src/main/java/com/example/mynotepad/DescriptionActivity.java
@@ -1,9 +1,11 @@
 package com.example.mynotepad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class DescriptionActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class DescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             finish();
@@ -26,5 +32,11 @@ public class DescriptionActivity extends AppCompatActivity {
                     .replace(R.id.description_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_description_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/mynotepad/MainActivity.java
+++ app/src/main/java/com/example/mynotepad/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mynotepad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean isLandscape = getResources().getConfiguration().
+                orientation == Configuration.ORIENTATION_LANDSCAPE;
+
+        getMenuInflater().inflate(isLandscape ? R.menu.title_menu_land :
+                R.menu.title_menu_port, menu);
+        return true;
     }
 }
